62a78da4539b4b79ac86037c7bc09d1b0d011caa
Kieu Quoc Chien <kieuquocchien@Kieu-Chien.local>
2018-05-17 21:22:07 +0900

    Init Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ticket Token
+
+Ticket Token is a token implementation for Ticket selling system
---
+++ src/BaseToken.sol
@@ -0,0 +1,146 @@
+pragma solidity ^0.4.16;
+
+/*  ERC 20 token */
+contract ERC20Interface {
+    function totalSupply() public view returns (uint);
+    function balanceOf(address _owner) public constant returns (uint256 balance);
+    function transfer(address _to, uint256 _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    function approve(address _spender, uint256 _value) public returns (bool success);
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+
+}
+contract BaseToken is ERC20Interface {
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18; // 18 decimals is the strongly suggested default, avoid changing it
+    uint256 public totalSupply;
+
+    // An array with all balances
+    mapping (address => uint256) public balances;
+    mapping (address => mapping (address => uint256)) allowed;
+
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
+
+    /**
+     * Constrctor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
+    constructor(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    ) public {
+        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+        balances[msg.sender] = totalSupply;                // Give the creator all initial tokens
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
+    }
+
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_to != 0x0);
+        if (balances[msg.sender] >= _value && _value > 0 && (balances[_to] + _value) > balances[_to]) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            emit Transfer(msg.sender, _to, _value);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens to `_to` in behalf of `_from`
+     *
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+      if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0 && balances[_to] + _value > balances[_to]) {
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        allowed[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    function balanceOf(address _owner) public constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    /**
+     * Set allowance for other address
+     *
+     * Allows `_spender` to spend no more than `_value` tokens in your behalf
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     */
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+      return allowed[_owner][_spender];
+    }
+
+    /**
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function burn(uint256 _value) public returns (bool success) {
+        if(balances[msg.sender] >= _value && _value > 0 ){
+            balances[msg.sender] -= _value;            // Subtract from the sender
+            totalSupply -= _value;                      // Updates totalSupply
+            emit Burn(msg.sender, _value);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        if(balances[_from] >= _value && _value > 0 && allowed[_from][msg.sender] > _value){
+            balances[_from] -= _value;                         // Subtract from the targeted balance
+            allowed[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+            totalSupply -= _value;                              // Update totalSupply
+            emit Burn(_from, _value);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
